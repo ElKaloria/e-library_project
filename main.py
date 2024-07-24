d2ff9824743c45f6835f75939b1b161cd1c09ef7
ElKaloria <maks.dektyarev@yandex.ru>
2024-07-24 22:05:49 +0700

    add try-except for empty file check, and search by title, author and year

--- main.py
+++ main.py
@@ -17,36 +17,50 @@ class Book:
 
 
 def write_book(book):
-    with open("library.json", "w+", encoding="cp1251") as file:
-        books = json.load(file)
-        if books is None:
+    with open("library.json", "r", encoding="cp1251") as file:
+        try:
+            books = json.load(file)
+            for b in books:
+                if b["id"] == book.id:
+                    book.id = b["id"] + 1
+        except json.decoder.JSONDecodeError:
             books = []
         books.append(book.__dict__)
-        json.dump(books, file, ensure_ascii=False, indent=4)
-
-
-def print_all_books():
-    with open("library.json", "r", encoding="cp1251") as file:
-        books = json.load(file)
-        for book in books:
-            print(Book(**book))
-
-
-def change_book_status(book_id, new_status):
-    with open("library.json", "r", encoding="cp1251") as file:
-        books = json.load(file)
-        for book in books:
-            if book["id"] == book_id:
-                book["status"] = new_status
     with open("library.json", "w", encoding="cp1251") as file:
         json.dump(books, file, ensure_ascii=False, indent=4)
 
 
+def print_all_books_json():
+    with open("library.json", "r", encoding="cp1251") as file:
+        try:
+            books = json.load(file)
+            for book in books:
+                print(Book(**book))
+        except json.decoder.JSONDecodeError:
+            print("Библиотека пуста.")
+
+
+def change_book_status(book_id, new_status):
+    with open("library.json", "r", encoding="cp1251") as file:
+        try:
+            books = json.load(file)
+            if book_id not in [b["id"] for b in books]:
+                print("Такой книги нет.")
+                return
+            for book in books:
+                if book["id"] == book_id:
+                    book["status"] = new_status
+        except json.decoder.JSONDecodeError:
+            print("Библиотека пуста.")
+    with open("library.json", "w", encoding="cp1251") as file:
+        json.dump(books, file, ensure_ascii=False, indent=4)
+
+def print_books(books):
+    for book in books:
+        print(Book(**book))
+
+
 if __name__ == "__main__":
-    # book = Book("The Catcher in the Rye", "J.D. Salinger", 1951, "В наличии")
-    # book2 = Book("War and Peace", "Leo Tolstoy", 1869, "В наличии")
-    # with open("library.json", "a", encoding="cp1251") as file:
-    #     json.dump(book2.__dict__, file, ensure_ascii=False, indent=4)
     while True:
         print(
             "Вас приветствует проект электронной библиотеки.\n"
@@ -66,29 +80,55 @@ if __name__ == "__main__":
                 book = Book(title, author, year, status)
                 write_book(book)
             case "2":
-                print_all_books()
+                print_all_books_json()
             case "3":
                 match input("1. По названию\n2. По автору\n3. По году\n"):
                     case "1":
                         title = input("Название: ")
                         with open("library.json", "r", encoding="cp1251") as file:
-                            books = json.load(file)
-                            filtred_books = filter(lambda x: x["title"] == title, books)
+                            try:
+                                books = json.load(file)
+                                filtred_books = list(filter(lambda x: x["title"] == title, books))
+                                if len(filtred_books) == 0:
+                                    print("Книги с таким названием нет.")
+                                else:
+                                    print_books(filtred_books)
+                            except json.decoder.JSONDecodeError:
+                                print("Библиотека пуста.")
                     case "2":
                         author = input("Автор: ")
                         with open("library.json", "r", encoding="cp1251") as file:
-                            books = json.load(file)
-                            filtred_books = filter(lambda x: x["author"] == author, books)
+                            try:
+                                books = json.load(file)
+                                filtred_books = list(filter(lambda x: x["author"] == author, books))
+                                if len(filtred_books) == 0:
+                                    print("Книг такого автора нет.")
+                                else:
+                                    print_books(filtred_books)
+                            except json.decoder.JSONDecodeError:
+                                print("Библиотека пуста.")
                     case "3":
                         year = int(input("Год: "))
                         with open("library.json", "r", encoding="cp1251") as file:
-                            books = json.load(file)
-                            filtred_books = filter(lambda x: x["year"] == year, books)
+                            try:
+                                books = json.load(file)
+                                filtred_books = list(filter(lambda x: x["year"] == year, books))
+                                if len(filtred_books) == 0:
+                                    print("Книг данного года нет.")
+                                else:
+                                    print_books(filtred_books)
+                            except json.decoder.JSONDecodeError:
+                                print("Библиотека пуста.")
             case "4":
                 del_id = input("Введите номер книги: ")
                 with open("library.json", "r", encoding="cp1251") as file:
-                    books = json.load(file)
-                    filtred_books = list(filter(lambda x: x["id"] == del_id, books))
+                    try:
+                        books = json.load(file)
+                        filtred_books = list(filter(lambda x: x["id"] != del_id, books))
+                        if len(filtred_books) == 0:
+                            print("Такои книги нет.")
+                    except json.decoder.JSONDecodeError:
+                        print("Библиотека пуста.")
                 with open("library.json", "w", encoding="cp1251") as file:
                     json.dump(filtred_books, file, ensure_ascii=False, indent=4)
             case "5":
